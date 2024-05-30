26986dae3cd0ee7d173b237ac6b19486378ff790
violaine-drt <viodarrent@gmail.com>
2024-05-30 11:50:05 +0200

    feat(money-card): destroyed money-card component, used ionic component for expenses and incomes in Homepage

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FlouzeLogoComponent } from './flouze-logo/flouze-logo.component';
 import { MainButtonComponent } from './main-button/main-button.component';
-import { MoneyCardComponent } from './money-card/money-card.component';
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [FlouzeLogoComponent, MainButtonComponent, MoneyCardComponent],
-  exports: [FlouzeLogoComponent, MainButtonComponent, MoneyCardComponent],
+  declarations: [FlouzeLogoComponent, MainButtonComponent],
+  exports: [FlouzeLogoComponent, MainButtonComponent],
 })
 export class ComponentsModule {}
