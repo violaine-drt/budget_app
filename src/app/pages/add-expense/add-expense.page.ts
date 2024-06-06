ca1fde39d16a71100b239fc0509160900bfcbd72
violaine-drt <viodarrent@gmail.com>
2024-06-06 16:11:22 +0200

    feat(add-expense form design): adding a multiple choices label in add expense form

--- src/app/pages/add-expense/add-expense.page.ts
+++ src/app/pages/add-expense/add-expense.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-expense',
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-expense.page.scss'],
 })
 export class AddExpensePage implements OnInit {
+  @Input() sort: string = '../../assets/icon/asterisk.svg';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
