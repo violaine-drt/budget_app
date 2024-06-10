fd73871902e0eab33e3d27d8f25d99a80f642e0c
violaine-drt <viodarrent@gmail.com>
2024-06-10 12:34:42 +0200

    feat(homepage) adding datepicking for date range when getting expenses & incomes

--- src/app/pages/homepage/homepage.page.ts
+++ src/app/pages/homepage/homepage.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.page.scss'],
 })
 export class HomepagePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  dateStart: string;
+  dateEnd: string;
+  showStartModal: boolean = false;
+  showEndModal: boolean = false;
+  constructor() {
+    const currentDate = new Date();
+    this.dateStart = currentDate.toISOString();
+    this.dateEnd = currentDate.toISOString();
+  }
+  updateDateStart(event: any) {
+    this.dateStart = event.detail.value;
   }
 
+  updateDateEnd(event: any) {
+    this.dateEnd = event.detail.value;
+  }
+  openStartModal() {
+    this.showStartModal = true;
+  }
+
+  closeStartModal() {
+    this.showStartModal = false;
+  }
+  openEndModal() {
+    this.showEndModal = true;
+  }
+
+  closeEndModal() {
+    this.showEndModal = false;
+  }
+
+  submitDates() {
+    console.log('Date de début:', this.dateStart);
+    console.log('Date de fin:', this.dateEnd);
+    if (new Date(this.dateStart) > new Date(this.dateEnd)) {
+      alert('La date de début doit être avant la date de fin.');
+    } else {
+      alert(`Période sélectionnée: Du ${this.dateStart} au ${this.dateEnd}`);
+    }
+  }
+  ngOnInit() {}
 }
