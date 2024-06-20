ae519e0ba69af42867cbc74f20bf7212c8efeef2
violaine-drt <viodarrent@gmail.com>
2024-06-20 16:16:26 +0200

    feat(history list): added dynamic rendering of transactions, using hard-coded data for now

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -46,6 +46,97 @@ export class HistoryPage implements OnInit {
       alert(`Période sélectionnée: Du ${this.dateStart} au ${this.dateEnd}`);
     }
   }
-
+  public Transactions = [
+    {
+      date: '11/01/2024',
+      id: 1,
+      amount: 70,
+      description: 'Courses hebdo',
+      supplier: 'Leclerc',
+      color: '#9FCCDA',
+      type: 'expense',
+    },
+    {
+      date: '11/01/2024',
+      id: 2,
+      amount: 5,
+      description: 'Achat fripes',
+      supplier: 'Ding Fring',
+      color: '#FF3D00',
+      type: 'expense',
+    },
+    {
+      date: '05/01/2024',
+      id: 3,
+      amount: 7,
+      description: 'Verre au PMU',
+      supplier: 'Le Balto',
+      color: '#FF008A',
+      type: 'expense',
+    },
+    {
+      date: '05/01/2024',
+      id: 4,
+      amount: 500,
+      description: 'Loyer',
+      supplier: 'Agence Immo',
+      color: '#306041',
+      type: 'expense',
+    },
+    {
+      date: '03/01/2024',
+      id: 5,
+      amount: 25,
+      description: 'Consultation',
+      supplier: 'Généraliste',
+      color: '#55578F',
+      type: 'expense',
+    },
+    {
+      date: '03/01/2024',
+      id: 6,
+      amount: 1000,
+      description: 'Indemnités chômage',
+      supplier: 'Pôle Emploi',
+      color: '#FFEB81',
+      type: 'income',
+    },
+    {
+      date: '03/01/2024',
+      id: 7,
+      amount: 10,
+      description: 'Achat lampe',
+      supplier: 'Emmaüs',
+      color: '#FF3D00',
+      type: 'expense',
+    },
+    {
+      date: '01/01/2024',
+      id: 8,
+      amount: 4.5,
+      description: 'Merguez VG',
+      supplier: 'Naturalia',
+      color: '#9FCCDA',
+      type: 'expense',
+    },
+    {
+      date: '01/01/2024',
+      id: 9,
+      amount: 40,
+      description: 'Electricité mensuelle',
+      supplier: 'GreenLight',
+      color: '#306050',
+      type: 'expense',
+    },
+    {
+      date: '01/01/2024',
+      id: 10,
+      amount: 40,
+      description: 'Electricité mensuelle',
+      supplier: 'Le Concorde',
+      color: '#FF008A',
+      type: 'expense',
+    },
+  ];
   ngOnInit() {}
 }
